d37adda48c73d0b0cf6f1ae78db1ce6391e93640
Hannes Hochreiner <hannes@hochreiner.net>
2017-01-21 20:51:17 +0100

    added more functions

--- antenna-design/src/antenna_design_utils.js
+++ antenna-design/src/antenna_design_utils.js
@@ -5,6 +5,7 @@
 let speedOfLight = 299792458; // m/s
 let conductivityOfCopper = 5.96e7; // S/m
 let permeabilityOfCopper = 1.256629e-6; // H/m
+let permeabilityVacuum = 4 * Math.PI * 1e-7; // H/m
 
 /** Calculate the effective radius of a trace.
 * @param {number} copperHeight height of the copper layer / m
@@ -25,4 +26,38 @@ export function calculateEffectiveRadius(copperHeight, traceWidth) {
 */
 export function calculateSecondaryLoopLength(frequency, seriesResistance, inputImpedance) {
   return Math.sqrt(seriesResistance * inputImpedance) / (frequency * permeabilityOfCopper * Math.log(9));
+}
+
+/** Calculate the radiation resistance.
+* @param {number} area area inclosed by the loop antenna / m²
+* @param {number} frequency frequency / Hz
+* @return {number} radiation resistance / Ohm
+*/
+export function calculateRadiationResistance(area, frequency) {
+  return 320 * Math.pow(Math.PI / speedOfLight * frequency, 4) * Math.pow(area, 2);
+}
+
+/** Calculate the loss resistance.
+* @param {number} perimeter perimeter of the loop antenna / m
+* @param {number} effectiveTraceRadius effective radius of the trace / m
+* @param {frequency} frequency / Hz
+* @return {number} loss resistance / Ohm
+*/
+export function calculateLossResistance(perimeter, effectiveTraceRadius, frequency) {
+  return perimeter * Math.sqrt(Math.PI * frequency * permeabilityOfCopper / conductivityOfCopper) / (2 * Math.PI * effectiveTraceRadius);
+}
+
+/** Calculate the inductance of a trace rectangle.
+* This calculation uses the equations given in Rosa, E.B. (1908). "The Self and Mutual Inductances of Linear Conductors". Bulletin of the Bureau of Standards. 4 (2): 301–344. [doi:10.6028/bulletin.088](https://dx.doi.org/10.6028%2Fbulletin.088).
+* @param {number} a side a of the rectangle / m
+* @param {number} b side b of the rectangle / m
+* @param {number} traceHeight height of the trace (i.e. of the copper layer) / m
+* @param {number} traceWidth width of the trace / m
+* @return {number} inductance / H
+*/
+export function calculateInductance(a, b, traceHeight, traceWidth) {
+  let sumAlphaBeta = (traceWidth + traceHeight);
+  let d = Math.sqrt(a * a + b * b);
+
+  return 4e-7 * ((a + b) * Math.log(2 * a * b / sumAlphaBeta) - a * Math.log(a + d) - b * Math.log(b + d) - (a + b) / 2 + 2 * d + 0.447 * sumAlphaBeta);
 }
--- antenna-design/src_test/antenna_design_utils.spec.js
+++ antenna-design/src_test/antenna_design_utils.spec.js
@@ -5,6 +5,30 @@ describe("antenna design util", () => {
     expect(adu.calculateEffectiveRadius(0.035e-3, 1e-3)).toBe(0.25225e-3);
   });
 
+  it("should calculate the radiation resistance", () => {
+    expect(Math.round(adu.calculateRadiationResistance(34e-3 * 12e-3, 434000000) * 1e4) / 1e4).toBe(0.0228);
+  });
+
+  it("should calculate the loss resistance", () => {
+    expect(Math.round(adu.calculateLossResistance(2 * (34e-3 + 12e-3), 0.25225e-3, 434000000) * 1e3) / 1e3).toBe(0.311);
+  });
+
+  it("should calculate the inductance", () => {
+    expect(Math.round(adu.calculateInductance(10, 10, 0.01, 0.01) * 1e8) / 1e8).toBe(55.53e-6);
+  });
+
+  it("should calculate the same inductance rotating the rectangle by 90 degrees", () => {
+    expect(adu.calculateInductance(5, 10, 0.01, 0.01)).toEqual(adu.calculateInductance(10, 5, 0.01, 0.01));
+  });
+
+  it("should calculate an inductance that is within 5% of other methods (Microchip AN831)", () => {
+    expect(Math.abs(adu.calculateInductance(34e-3, 12e-3, 0.035e-3, 1e-3) / 65.67e-9 - 1)).toBeLessThan(0.05);
+  });
+
+  it("should calculate an inductance that is within 5% of other methods (Silicon Labs AN639)", () => {
+    expect(Math.abs(adu.calculateInductance(40e-3, 25e-3, 0.035e-3, 1e-3) / 102.64e-9 - 1)).toBeLessThan(0.05);
+  });
+
   it("should calculate the secondary loop length", () => {
     expect(Math.round(adu.calculateSecondaryLoopLength(434000000, 0.275, 1000) * 1e4) / 1e4).toBe(13.8e-3);
   });
