d2ce790708d77759ab3734b0851dfdf70caa88fe
Hannes Hochreiner <hannes@hochreiner.net>
2017-01-21 18:13:47 +0100

    added function to calculate the secondary loop length

--- antenna-design/src/antenna_design_utils.js
+++ antenna-design/src/antenna_design_utils.js
@@ -2,10 +2,27 @@
 * @module antenna_design_utils
 */
 
+let speedOfLight = 299792458; // m/s
+let conductivityOfCopper = 5.96e7; // S/m
+let permeabilityOfCopper = 1.256629e-6; // H/m
+
 /** Calculate the effective radius of a trace.
-* @param {number} copperHeight height of the copper layer in meters
-* @param {number} traceWidth width of the trace in meters
+* @param {number} copperHeight height of the copper layer / m
+* @param {number} traceWidth width of the trace / m
+* @return {number} effective radius / m
 */
 export function calculateEffectiveRadius(copperHeight, traceWidth) {
   return 0.35 * copperHeight + 0.24 * traceWidth;
+}
+
+/** Calculate the secondary loop length.
+* This function assumes that the shorter side of the loop is twice the trace
+* width.
+* @param {number} frequency frequency / Hz
+* @param {number} seriesResistance series resistance of the loop / Ohm
+* @param {number} inputImpedance impedance of the source / Ohm
+* @return {number} secondary loop length / m
+*/
+export function calculateSecondaryLoopLength(frequency, seriesResistance, inputImpedance) {
+  return Math.sqrt(seriesResistance * inputImpedance) / (frequency * permeabilityOfCopper * Math.log(9));
 }
--- antenna-design/src_test/antenna_design_utils.spec.js
+++ antenna-design/src_test/antenna_design_utils.spec.js
@@ -1,7 +1,11 @@
 import * as adu from "../bld/antenna_design_utils";
 
 describe("antenna design util", () => {
-  it("fail on this test", () => {
+  it("calculate the effective radius", () => {
     expect(adu.calculateEffectiveRadius(0.035e-3, 1e-3)).toBe(0.25225e-3);
+  });
+
+  it("should calculate the secondary loop length", () => {
+    expect(Math.round(adu.calculateSecondaryLoopLength(434000000, 0.275, 1000) * 1e4) / 1e4).toBe(13.8e-3);
   });
 });
