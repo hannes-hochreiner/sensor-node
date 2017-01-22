ef619eebf69b1e53aba36d81c7303b4882a5e926
Hannes Hochreiner <hannes@hochreiner.net>
2017-01-22 19:09:13 +0100

    switched formatting

--- antenna-design/src/index.js
+++ antenna-design/src/index.js
@@ -1,4 +1,7 @@
 import * as adu from "./antenna_design_utils";
+import * as d3f from "d3-format";
+
+let f = d3f.format(".3s");
 
 console.log("Antenna Design");
 let sideA = 25e-3;
@@ -6,43 +9,43 @@ let sideB = 25e-3;
 let width = 1.5e-3;
 let height = 70e-6;
 
-console.log(`loop size: ${sideA * 1e3} mm x ${sideB * 1e3} mm`);
-console.log(`trace: ${width * 1e3} mm x ${height * 1e6} µm`);
+console.log(`loop size: ${f(sideA)}m x ${f(sideB)}m`);
+console.log(`trace: ${f(width)}m x ${f(height)}m`);
 
 let frequency = 434e6;
-console.log(`frequency: ${frequency / 1e6} MHz`);
+console.log(`frequency: ${f(frequency)}Hz`);
 
 let inputImpedance = 600;
-console.log(`input impedance: ${inputImpedance} Ohm`);
+console.log(`input impedance: ${f(inputImpedance)}Ω`);
 
 let qFactor = 350;
 console.log(`q factor: ${qFactor}`);
 
 let inductance = adu.calculateInductance(sideA, sideB, height, width);
-console.log(`inductance: ${inductance * 1e9} nH`);
+console.log(`inductance: ${f(inductance)}H`);
 
 let capacitance = adu.calculateCapacitance(inductance, frequency);
-console.log(`capacitance: ${capacitance * 1e12} pC`);
+console.log(`capacitance: ${f(capacitance)}C`);
 
 let radR = adu.calculateRadiationResistance(sideA * sideB, frequency);
-console.log(`radiation resistance: ${radR * 1e3} mOhm`);
+console.log(`radiation resistance: ${f(radR)}Ω`);
 
 let traceR = adu.calculateTraceResistance(2 * (sideA + sideB), 2 * width, frequency);
-console.log(`trace resistance: ${traceR * 1e3} mOhm`);
+console.log(`trace resistance: ${f(traceR)}Ω`);
 
 let srM = radR + traceR + adu.calculateEquivalentSeriesResistance(capacitance, frequency, qFactor);
-console.log(`series resistance (Microchip): ${srM} Ohm`);
-console.log(`gain (Microchip): ${10 * Math.log10(radR / srM)} dB`);
+console.log(`series resistance (Microchip): ${f(srM)}Ω`);
+console.log(`gain (Microchip): ${f(10 * Math.log10(radR / srM))}dB`);
 
 let sllM = adu.calculateSecondaryLoopLength(frequency, srM, inputImpedance);
-console.log(`secondary loop (Microchip): ${2e3 * width} mm x ${sllM * 1e3} mm`);
+console.log(`secondary loop (Microchip): ${f(2 * width)}m x ${f(sllM)}m`);
 
 let radRS = adu.calculateRadiationResistance(sideA * sideB, frequency, 0.82);
-console.log(`radiation resistance (Silicon Labs): ${radRS * 1e3} mOhm`);
+console.log(`radiation resistance (Silicon Labs): ${f(radRS)}Ω`);
 
 let srS = radRS + traceR + 0.4 + adu.calculateEquivalentSeriesResistance(capacitance, frequency, qFactor);
-console.log(`series resistance (Silicon Labs): ${srS} Ohm`);
-console.log(`gain (Silicon Labs): ${10 * Math.log10(radRS / srS)} dB`);
+console.log(`series resistance (Silicon Labs): ${f(srS)}Ω`);
+console.log(`gain (Silicon Labs): ${f(10 * Math.log10(radRS / srS))}dB`);
 
 let sllS = adu.calculateSecondaryLoopLength(frequency, srS, inputImpedance);
-console.log(`secondary loop (Silicon Labs): ${2e3 * width} mm x ${sllS * 1e3} mm`);
+console.log(`secondary loop (Silicon Labs): ${f(2 * width)}m x ${f(sllS)}m`);
